0c888fd8bcfef872457d1c6bd3d26dc37692cf9a
alex-pf <prizrakfantezi@gmail.com>
2015-02-15 16:54:09 +0300

    Поправил тесты первого задания первого урока. Переэкспортировал их в java.

--- urok2/src/test/java/ru/st/selenium/AddAndRmNewDvd.java
+++ urok2/src/test/java/ru/st/selenium/AddAndRmNewDvd.java
@@ -10,12 +10,12 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class AddNewDvd extends ru.st.selenium.pages.TestBase {
+public class AddAndRmNewDvd extends ru.st.selenium.pages.TestBase {
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
 
   @Test
-  public void testAddNewDvd() throws Exception {    // Проверка функционала добавления фильма в коллекцию
+  public void testAddAndRmNewDvd() throws Exception {    // Проверка функционала добавления фильма в коллекцию
     // К началу теста мы должны быть авторизованы и на домашней странице
     driver.get(baseUrl + "/php4dvd/");
     driver.findElement(By.cssSelector("img[alt=\"Add movie\"]")).click();
@@ -44,6 +44,19 @@ public class AddNewDvd extends ru.st.selenium.pages.TestBase {
     // Убедимся, что фильм виден на главной странице
     assertTrue(isElementPresent(By.xpath("//div[@id='results']/a/div")));
     assertEquals("Страшилка", driver.findElement(By.xpath("//a/div/div[2]")).getText());
+    // Проверка функционала удаления фильма из коллекции
+    // До начала теста мы должны быть авторизованы, фильм должен быть в коллекции, начало теста на домашней странице
+    driver.findElement(By.xpath("//a/div/div/div")).click();
+    driver.findElement(By.cssSelector("img[alt=\"Remove\"]")).click();
+    // Подтвердили что хотим удалить фильм
+    assertTrue(closeAlertAndGetItsText().matches("^Are you sure you want to remove this[\\s\\S]$"));
+    // Вернулись на домашнюю страницу
+    driver.findElement(By.linkText("Home")).click();
+    // Убедились - фильмов в коллекции нет, обновили данные, проверили ещё раз
+    // ERROR: Caught exception [ERROR: Unsupported command [getElementIndex | //div[@id='results']/a/div | ]]
+    driver.findElement(By.cssSelector("img[alt=\"Update all\"]")).click();
+    // ERROR: Caught exception [ERROR: Unsupported command [getElementIndex | //div[@id='results']/a/div | ]]
+    driver.findElement(By.linkText("Home")).click();
   }
 
   private boolean isElementPresent(By by) {
